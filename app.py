cbd0c185ce88a52aec326bfbf1820b1d438b842e
gopavaramkushal37 <gopavaramkushal37@gmail.com>
2026-08-09 14:43:11 +0200

    Test CI failure

--- app.py
+++ app.py
@@ -3,12 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def home(
     return "Hello World!"
-
-@app.route("/about")
-def about():
-    return "This is the About page."
 
 if __name__ == "__main__":
     app.run()
