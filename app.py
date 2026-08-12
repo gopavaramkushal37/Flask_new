2c6fab109654c466d4fbe8f3303684801739a166
gopavaramkushal37 <gopavaramkushal37@gmail.com>
2026-08-12 18:26:13 +0200

    New changes

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return "Hello World!"
 
+@app.route("/home_page")
+def home_page():
+    return "Home Page"
+
+@app.route("/about")
+def about():
+    return "About Page"
+
+@app.route("/contact")
+def contact():
+    return "Contact Page"
+
 if __name__ == "__main__":
     app.run()
--- test_app.py
+++ test_app.py
@@ -7,3 +7,19 @@ def test_home():
 
     assert response.status_code == 200
     assert response.data == b"Hello World!"
+
+def test_about():
+    client = app.test_client()
+
+    response = client.get("/about")
+
+    assert response.status_code == 200
+    assert response.data == b"About Page"
+
+def test_contact():
+    client = app.test_client()
+
+    response = client.get("/contact")
+
+    assert response.status_code == 200
+    assert response.data == b"Contact Page"
